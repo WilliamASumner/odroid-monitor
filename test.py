8fddf677f9f64da21c28ba80f54990dd877e20f2
Will <williamasumner@gmail.com>
2019-06-18 19:45:14 +0000

    added simple testing script

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from time import sleep
+from sys import argv
+x = 30
+try:
+	x = int(argv[1])
+except:
+	pass
+while (x > 0):
+	print(str(x) + "...")
+	x -= 1
+	sleep(1)
+print("DONE")
