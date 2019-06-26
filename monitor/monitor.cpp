6e7342855b360b9b32686eeaa79878a48dc13c13
WilliamASumner <williamasumner@gmail.com>
2019-06-26 10:08:24 -0400

    added mode

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -107,11 +107,11 @@ int get_power(struct odroid_state * state, cbuf_handle_t handle) {
             raw_data[9] = 0; // null terminate
             if (circular_buf_put_bytes(handle,(u_int8_t *)raw_data, strlen(raw_data)) != strlen(raw_data)) {
                 fprintf(stderr,"error: unable to write to buffer\n");
-                return -2;
+                return -1;
             }
         }
     }
-    return 1;
+    return 0;
 }
 
 int get_cid(int pid,int tid) { // opens stats file and gets cid of task
@@ -143,7 +143,7 @@ int get_cid(int pid,int tid) { // opens stats file and gets cid of task
             cid = std::stoi(line.substr(prev,index-prev));
         else {
 #ifdef DEBUG
-            fprintf(stderr,"error: the index was not found\n");
+            fprintf(stderr,"error: the index of the CID was not found\n");
 #endif
             stat_file.close();
             return -1;
@@ -166,9 +166,9 @@ int get_timestamp(cbuf_handle_t handle) { // just writes the timestamp
     snprintf(time_str,100,"%ld.%06ld:",retrieve.tv_sec, retrieve.tv_usec); // write down the time of measurement in the standard format
     if (circular_buf_put_bytes(handle,(u_int8_t *)time_str, strlen(time_str)) != strlen(time_str)) {
         fprintf(stderr,"error: unable to write to buffer\n");
-        return -2;
+        return -1;
     }
-    return 1;
+    return 0;
 }
 
 int get_cpu_config(int proc_pid, cbuf_handle_t handle) { // writes the cpu_config
@@ -192,13 +192,13 @@ int get_cpu_config(int proc_pid, cbuf_handle_t handle) { // writes the cpu_confi
 #ifdef DEBUG
                 fprintf(stderr,"error: unable to get core id\n");
 #endif
-        continue;
+                continue;
             }
             snprintf(cid_str,sizeof(cid_str),"(%d,%d);",tid,cid); // write tid,cid pairs
             if (circular_buf_put_bytes(handle,(u_int8_t *)cid_str, strlen(cid_str)) != strlen(cid_str)) {
                 fprintf(stderr,"error: unable to write to buffer\n");
                 closedir(proc_dir);
-                return -2;
+                return -1;
             }
         }
     } else {
@@ -206,15 +206,15 @@ int get_cpu_config(int proc_pid, cbuf_handle_t handle) { // writes the cpu_confi
         fprintf(stderr,"error: unable to read process directory %s\n",dirname);
 #endif
         closedir(proc_dir);
-        return -2;
+        return -1;
     }
     if (circular_buf_put_byte_checked(handle,'\n') != 0) { // write \n between lists
         fprintf(stderr,"error: unable to write to buffer\n");
         closedir(proc_dir);
-        return -2;
+        return -1;
     }
     closedir(proc_dir);
-    return 1;
+    return 0;
 }
 
 void sig_handler(int signo) { 
@@ -229,8 +229,49 @@ void sig_handler(int signo) {
             break;
         default:
             fprintf(stderr,"error: improper signal found, unable to handle\n");
-            exit(1);
+            exit(-1);
     }
+}
+
+int collect_stats(cbuf_handle_t file_handle, char mode, int pid, struct odroid_state * state) {
+    printf("collecting stats\n");
+    switch (mode) {
+        case 'e': // energy
+            if (get_timestamp(file_handle) != 0) // TODO clean this up
+                return -1;
+            break;
+        case 'c': // cpu config
+            if (get_cpu_config(pid,file_handle) != 0 )
+                return -1;
+            break;
+        case 'b': // both
+            if (get_power(state,file_handle) != 0)
+                return -1;
+            if (get_cpu_config(pid,file_handle) != 0)
+                return -1;
+        default:
+            break;
+    }
+    return 0;
+}
+
+void free_circ_buffer(cbuf_handle_t handle) {
+    if (handle) {
+        circular_buf_write(handle); // write out the data
+        if (handle->file) {
+            fclose(handle->file); // close the file
+            handle->file = NULL;
+        }
+        if (handle->buffer) {
+            free(handle->buffer); // free the buffer
+            handle->buffer = NULL;
+        }
+        circular_buf_free(handle); // free the handle
+    }
+}
+
+void print_usage(const char * name) {
+        fprintf(stderr,"usage: %s [interval] [pid to attach or -1] [mode] [log-name] [command...]\n",name);
 }
 
 int main(int argc, char **argv) {
@@ -243,54 +284,92 @@ int main(int argc, char **argv) {
 
     if (sigaction(SIGINT,&new_action,NULL) == -1) {
         fprintf(stderr,"error: cannot catch SIGINT\n");
+        return -1;
     }
     if (sigaction(SIGTERM,&new_action,NULL) == -1) {
         fprintf(stderr,"error: cannot catch SIGTERM\n");
+        return -1;
     }
 
-    int buffer_size = 104857600; // 100 mb, maybe change this
-    char out_filename[] = "/tmp/monitor-results";
+    // Read in values from command line
+    // ./monitor [interval] [pid or -1 for attach] [mode] [log-name] command
+    //
+    if (argc < 5) {
+        fprintf(stderr,"error: not enough arguments\n");
+        print_usage(argv[0]);
+        return -1;
+    }
+
+    struct timespec del,rem; // first arg
+    del.tv_sec = 0;
+    del.tv_nsec = 1000L * atol(argv[1]); // convert us seconds to proper ns
+
+    int pid = atoi(argv[2]); // second arg
+    int will_attach = 0;
+
+    int mode = argv[3][0]; // third arg
+
+    char out_filename[100];
+    if (strnlen(argv[4],101) > 100) {
+        fprintf(stderr,"error: outfile name is too long (> 100 chars)\n");
+        return -1;
+    }
+    strncpy(out_filename,argv[4],100); // fourth arg
+
     FILE * file = fopen (out_filename, "w");
     if (!file) {
         fprintf(stderr,"error: could not open '%s', Error %d:%s\n",out_filename,errno,strerror(errno));
-        exit(1);
+        return -1;
     }
-    u_int8_t * buffer = (u_int8_t *)malloc(sizeof(u_int8_t)*buffer_size);
-    char **new_argv;
+
+    // Initialize the monitor state
+    int buffer_size = 104857600; // 100 mb buffer limit
+    u_int8_t * buffer = (u_int8_t *)malloc(sizeof(u_int8_t) * buffer_size);
     char prog_name[100];
-    char header_str[] = "Timestamp:SENS_A7;SENS_A15;SENS_MEM;SENS_GPU;(TID,CORE_ID);[(TID,CORE_ID);...]\n";
-    cbuf_handle_t cpu_handle = circular_buf_init(buffer,buffer_size,file);
-    if (circular_buf_put_bytes(cpu_handle,(u_int8_t *)header_str, strlen(header_str)) != strlen(header_str)) {
-        fprintf(stderr,"error writing to results file\n");
-        circular_buf_free(cpu_handle);
-        exit(1);
+    char header_str[100];
+    cbuf_handle_t file_handle = circular_buf_init(buffer,buffer_size,file);
+
+    switch(mode) {
+        case 'e': 
+            strncpy(header_str,"Timestamp:SENS_A7;SENS_A15;SENS_MEM;SENS_GPU;\n",100);
+            break;
+        case 'c':
+            strncpy(header_str,"Timestamp:(TID,CORE_ID);[(TID,CORE_ID);...]\n",100);
+            break;
+        case 'b':
+            strncpy(header_str,"Timestamp:SENS_A7;SENS_A15;SENS_MEM;SENS_GPU;(TID,CORE_ID);[(TID,CORE_ID);...]\n",100);
+            break;
+        default:
+            fprintf(stderr,"error: invalid mode supplied, must be e, c, or b\n");
+            return -1;
     }
 
-    // ARGS settings
+    // Try writing to the circular buffer
+    if (circular_buf_put_bytes(file_handle,(u_int8_t *)header_str, strlen(header_str)) != strlen(header_str)) {
+        fprintf(stderr,"error writing to results buffer\n");
+        return -1;
+    }
 
-    int num_args_to_skip = 3; //./prog_name [interval] [pid] command
+    // Processing
+    char **new_argv;
+    int num_args_to_skip = 5;
     if (argc - num_args_to_skip < 0 ) {
-        fprintf(stderr,"usage: %s [interval] [pid to attach or -1] [command...]\n",argv[0]);
-        exit(1);
+        print_usage(argv[0]);
+        free_circ_buffer(file_handle);
+        return -1;
     }
-    struct timespec del,rem;
-    del.tv_sec = 0;
-    del.tv_nsec = 1000L * atol(argv[1]); // convert us seconds to proper ns
-    int pid = -1;
-    int will_attach = 0;
 
-    struct odroid_state * state = (struct odroid_state *)malloc(sizeof(struct odroid_state));
-    init_odroid_state(state); // start up sensors
-
-
-    if (atoi(argv[2]) < 1) { // supplied a non-valid pid
-        new_argv = (char **)malloc((argc-num_args_to_skip)*sizeof(char *)); // ./name and interval are removed
+    if (pid == -1) { // user wants to attach to new pid from command rather than existing pid
+        new_argv = (char **)malloc((argc-num_args_to_skip)*sizeof(char *)); // new arglist
         if (new_argv == NULL) {
             fprintf(stderr,"Error: error allocating memory\n");
-            exit(1);
-        } else if (argc < 3) {
-            fprintf(stderr,"Error: incorrect number of inputs\n");
-            exit(1);
+            free_circ_buffer(file_handle);
+            return -1;
+        } else if (argc < 3 || strnlen(argv[num_args_to_skip],101) > 100) {
+            fprintf(stderr,"Error: improperly formatted command\n");
+            print_usage(argv[0]);
+            free_circ_buffer(file_handle);
+            return -1;
         }
         strncpy(prog_name,argv[num_args_to_skip],100);
 
@@ -299,66 +378,73 @@ int main(int argc, char **argv) {
             if (new_argv[i-num_args_to_skip] == NULL)  {
                 fprintf(stderr, "Error copying new argv array\n");
                 free_args(new_argv,i-num_args_to_skip); // just cleanup what we can
-                exit(1);
+                free_circ_buffer(file_handle);
+                return -1;
             }
             strncpy(new_argv[i-num_args_to_skip],argv[i],100);
         }
-        new_argv[argc-num_args_to_skip] = (char *)NULL; // terminated by NULL
+        new_argv[argc-num_args_to_skip] = (char *)NULL; // arglist must be terminated by NULL
         pid = fork();
-    } else {
-        pid = atoi(argv[2]);
+    } else if (atoi(argv[2]) > 0) {
         will_attach = 1;
+    } else {
+        fprintf(stderr,"error: invalid ID supplied\n");
+        free_circ_buffer(file_handle);
+        return -1;
     }
-
 
     if (pid == 0) { // child
         print_args(new_argv,argc-num_args_to_skip);
         execvp(prog_name,new_argv);
         fprintf(stderr,"unable to execute program %s\n",prog_name);
-        exit(1);
-
+        return -1;
     } else { // parent of the fork
+#ifdef __linux__
         cpu_set_t mask;
         CPU_ZERO(&mask);
-        CPU_SET(7,&mask); // set to same big core every time to increase predictablity
+        CPU_SET(0,&mask); // set to same big core every time to increase predictablity
         sched_setaffinity(0,sizeof(mask),&mask);
 
         if (will_attach) {
-            if (ptrace(PTRACE_SEIZE,pid, NULL, NULL) == -1){ // attach to that guy, but dont stop it
+            if (ptrace(PTRACE_SEIZE,pid, NULL, NULL) == -1){ // attach to that pid, but dont stop it
                 fprintf(stderr,"error: could not seize process: %d\n",pid);
                 fprintf(stderr,"try running as root, alternatively process may not exist\n");
-                circular_buf_free(cpu_handle);
-                fclose(file);
-                free(buffer);
+                free_circ_buffer(file_handle,power_handle);
                 return -1;
 
             }
             printf("succesfully attached\n");
         }
-
+#else
+        if (will_attach) {
+            fprintf(stderr,"error: attach functionality not yet implemented for this system\n");
+            free_circ_buffer(file_handle);
+            return -1;
+        }
+#endif
 
         int status;
         pid_t return_pid = waitpid(pid, &status, WNOHANG);
-        int all_good = 1;
+        int error = 1;
+        struct odroid_state * state = (struct odroid_state *)malloc(sizeof(struct odroid_state));
+        if (mode != 'c') // if not cpu_config_only
+            init_odroid_state(state);
 
-        while ((return_pid = waitpid(pid,&status,WNOHANG)) == 0 && all_good != -2 && !signal_cleanup) { // while the thread isnt done
-            if ((all_good = get_timestamp(cpu_handle)) == -2) // TODO clean this up
-                break;
-            if ((all_good = get_power(state,cpu_handle)) == -2)
-                break;
-            all_good = get_cpu_config(pid,cpu_handle);
+
+        while ((return_pid = waitpid(pid,&status,WNOHANG)) == 0 && !error && !signal_cleanup) { // while the thread isnt done
+            error = collect_stats(file_handle,mode,pid,state);
             nanosleep(&del,&rem); // sleep for the requisite amount of time
         }
         if (return_pid < 0 && !will_attach )
             fprintf(stderr,"Error running child process\n");
 
         // cleanup!
-        circular_buf_free(cpu_handle);
+        free_circ_buffer(file_handle);
         //if (!will_attach)
         //  free_args(new_argv,argc - num_args_to_skip); // not sure if these needs to be done?
-        end_odroid_state(state);
+        if (mode != 'c')
+            end_odroid_state(state);
         free(state);
-        fclose(file);
     }
     return 0;
 }
